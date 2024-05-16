9a498e210c173bc61bc9557d2667986da3f8c800
john <john@primitive.io>
2024-05-16 17:22:04 +0200

    Initial commit

---
+++ Assets/Main.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NAudio.Midi;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public string midiPath;
+
+    const float noteScale = .01f;
+
+    public void Awake()
+    {
+        // Load the MIDI file
+        MidiFile midi = new(midiPath, false);
+
+        for (int trackIndex = 0; trackIndex < midi.Events.Count(); trackIndex++)
+        {
+            IList<MidiEvent> trackEvents = midi.Events[trackIndex];
+
+            int totalNoteCount = 0;
+
+            foreach (MidiEvent trackEvent in trackEvents)
+            {
+                if (totalNoteCount > 50) continue;
+
+                if (trackEvent is NoteOnEvent noteOnEvent &&
+                    noteOnEvent.Velocity > 0) // Only consider NoteOn with velocity > 0 (actual note starts)
+                {
+                    int noteKey = noteOnEvent.NoteNumber;
+                    long onAbsoluteTime = noteOnEvent.AbsoluteTime;
+                    long offAbsoluteTime = noteOnEvent.OffEvent.AbsoluteTime;
+                    long duration = offAbsoluteTime - onAbsoluteTime;
+
+                    GameObject noteGo = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    noteGo.transform.SetParent(transform, false);
+
+                    noteGo.transform.localScale = new Vector3(.2f, .001f, duration * noteScale);
+                    noteGo.transform.Translate(Vector3.right * noteKey * .5f);
+                    noteGo.transform.Translate(Vector3.forward * (onAbsoluteTime + duration * .5f) * noteScale);
+                    totalNoteCount++;
+                }
+            }
+        }
+    }
+}
